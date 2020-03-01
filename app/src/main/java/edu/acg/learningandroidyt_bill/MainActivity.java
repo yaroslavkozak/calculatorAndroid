936375cc39a0e12dc2b0fb10f6ded55876028c3d
Yaroslav Kozak <yaroslavkozak@Yaroslavs-MacBook-Pro.local>
2020-03-01 16:22:05 +0200

    Initial commit

---
+++ app/src/main/java/edu/acg/learningandroidyt_bill/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.acg.learningandroidyt_bill;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+
+                int result = num1 + num2;
+
+                resultTextView.setText(result + " ");
+
+
+
+
+            }
+        });
+
+    }
+}
